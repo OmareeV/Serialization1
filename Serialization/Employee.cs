4e370bc17e6dbe2c40d96887e1a0bed18f149bc9
Valentin_Omaree <Valentin_Omaree@edu.mcctc.net>
2020-10-29 13:28:54 -0400

    Add project files.

---
+++ Serialization/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+    [Serializable]
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+
+        public double SSNumber { get; set; }
+
+        public DateTime EntryDate { get; set; }
+    }
+}
---
+++ Serialization/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml; 
+using System.Xml.Serialization;
+using System.IO;
+
+namespace Serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+
+            emp.ID = 123;
+            emp.name = "Nerd";
+            emp.SSNumber = 123456789;
+            emp.EntryDate = DateTime.Now;
+
+            Console.WriteLine($"Employee ID: {emp.ID} \nEmployee Name: {emp.name}\nEmployee SS# {emp.SSNumber}\nDate Created:{emp.EntryDate}");
+        }
+    }
+}
